58ac6b63e8c9adfa317a66eec4606994538a75fa
Daneyon Hansen <daneyonhansen@gmail.com>
2020-09-14 20:25:41 -0700

    Implements Contour Namespace API
    Signed-off-by: Daneyon Hansen <daneyonhansen@gmail.com>

--- controller/contour/controller.go
+++ controller/contour/controller.go
@@ -71,7 +71,7 @@ func (r *Reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	// The contour is safe to process, so ensure current state matches desired state.
 	desired := contour.ObjectMeta.DeletionTimestamp.IsZero()
 	if desired {
-		if err := r.ensureContour(ctx, contour, defaultContourNamespace); err != nil {
+		if err := r.ensureContour(ctx, contour); err != nil {
 			switch e := err.(type) {
 			case retryable.Error:
 				r.Log.Error(e, "got retryable error; requeueing", "after", e.After())
@@ -81,7 +81,7 @@ func (r *Reconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 			}
 		}
 	} else {
-		if err := r.ensureContourRemoved(ctx, contour, defaultContourNamespace); err != nil {
+		if err := r.ensureContourRemoved(ctx, contour); err != nil {
 			switch e := err.(type) {
 			case retryable.Error:
 				r.Log.Error(e, "got retryable error; requeueing", "after", e.After())
@@ -101,28 +101,58 @@ func (r *Reconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Complete(r)
 }
 
-// ensureContour ensures all necessary resources exist for the given contour
-// in namespace ns.
-func (r *Reconciler) ensureContour(ctx context.Context, contour *operatorv1alpha1.Contour, ns string) error {
+// ensureContour ensures all necessary resources exist for the given contour.
+func (r *Reconciler) ensureContour(ctx context.Context, contour *operatorv1alpha1.Contour) error {
 	// Before doing anything with the contour, ensure it has a finalizer
 	// so it can cleaned-up later.
 	if err := r.ensureFinalizer(ctx, contour); err != nil {
 		return fmt.Errorf("failed to finalize contour %s/%s: %w", contour.Namespace, contour.Name, err)
 	}
-	if err := r.ensureNamespace(ctx, ns); err != nil {
-		return fmt.Errorf("failed to ensure namespace %s: %w", ns, err)
+	if err := r.ensureNamespace(ctx, contour); err != nil {
+		return fmt.Errorf("failed to ensure namespace %s for contour %s/%s: %w",
+			contour.Spec.Namespace.Name, contour.Namespace, contour.Name, err)
 	}
 	return nil
 }
 
-// ensureContourRemoved ensures all resources for the given contour do not exist
-// in namespace ns.
-func (r *Reconciler) ensureContourRemoved(ctx context.Context, contour *operatorv1alpha1.Contour, ns string) error {
-	if err := r.ensureNamespaceRemoved(ctx, defaultContourNamespace); err != nil {
-		return fmt.Errorf("failed to remove namespace %s: %w", defaultContourNamespace, err)
+// ensureContourRemoved ensures all resources for the given contour do not exist.
+func (r *Reconciler) ensureContourRemoved(ctx context.Context, contour *operatorv1alpha1.Contour) error {
+	if err := r.ensureNamespaceRemoved(ctx, contour); err != nil {
+		return fmt.Errorf("failed to remove namespace %s for contour %s/%s: %w",
+			contour.Spec.Namespace.Name, contour.Namespace, contour.Name, err)
 	}
 	if err := r.ensureFinalizerRemoved(ctx, contour); err != nil {
 		return fmt.Errorf("failed to remove finalizer from contour %s/%s: %w", contour.Namespace, contour.Name, err)
 	}
 	return nil
+}
+
+// otherContoursExist lists Contour objects in all namespaces, returning the list
+// and true if any exist other than contour.
+func (r *Reconciler) otherContoursExist(ctx context.Context, contour *operatorv1alpha1.Contour) (bool, *operatorv1alpha1.ContourList, error) {
+	contours := &operatorv1alpha1.ContourList{}
+	if err := r.Client.List(ctx, contours); err != nil {
+		return false, nil, fmt.Errorf("failed to list contours: %w", err)
+	}
+	if len(contours.Items) == 0 || len(contours.Items) == 1 && contours.Items[0].Name == contour.Name {
+		return false, nil, nil
+	}
+	return true, contours, nil
+}
+
+// otherContoursExistInSpecNs lists Contour objects in the same spec.namespace.name as contour,
+// returning true if any exist.
+func (r *Reconciler) otherContoursExistInSpecNs(ctx context.Context, contour *operatorv1alpha1.Contour) (bool, error) {
+	exist, contours, err := r.otherContoursExist(ctx, contour)
+	if err != nil {
+		return false, err
+	}
+	if exist {
+		for _, c := range contours.Items {
+			if c.Spec.Namespace.Name == contour.Spec.Namespace.Name {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
 }
