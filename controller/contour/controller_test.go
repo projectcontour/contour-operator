b74de82b6d61d42aa4c5364eed26defa228bddca
Daneyon Hansen <daneyonhansen@gmail.com>
2020-09-24 20:24:17 -0700

    Adds Initial Integration Testing
    Signed-off-by: Daneyon Hansen <daneyonhansen@gmail.com>

---
+++ controller/contour/controller_test.go
@@ -0,0 +1,118 @@
+/*
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package contour
+
+import (
+	"context"
+
+	operatorv1alpha1 "github.com/projectcontour/contour-operator/api/v1alpha1"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("Run controller", func() {
+
+	Context("When creating a contour", func() {
+		It("Defaults should be set", func() {
+			ctx := context.Background()
+
+			key := types.NamespacedName{
+				Name:      contourName,
+				Namespace: operatorNamespace,
+			}
+
+			By("By creating a contour with a nil spec")
+			created := &operatorv1alpha1.Contour{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: key.Namespace,
+					Name:      key.Name,
+				},
+			}
+			Expect(k8sClient.Create(ctx, created)).Should(Succeed())
+
+			By("Expecting default replicas")
+			Eventually(func() int32 {
+				f := &operatorv1alpha1.Contour{}
+				k8sClient.Get(ctx, key, f)
+				return f.Spec.Replicas
+			}, timeout, interval).Should(Equal(int32(defaultReplicas)))
+
+			By("Expecting default namespace")
+			Eventually(func() string {
+				f := &operatorv1alpha1.Contour{}
+				k8sClient.Get(ctx, key, f)
+				return f.Spec.Namespace.Name
+			}, timeout, interval).Should(Equal(defaultNamespace))
+
+			By("Expecting default remove namespace on deletion")
+			Eventually(func() bool {
+				f := &operatorv1alpha1.Contour{}
+				k8sClient.Get(ctx, key, f)
+				return f.Spec.Namespace.RemoveOnDeletion
+			}, timeout, interval).Should(Equal(false))
+
+			// Update the contour
+			By("By updating a contour spec")
+			updated := &operatorv1alpha1.Contour{}
+			Expect(k8sClient.Get(ctx, key, updated)).Should(Succeed())
+			updatedReplicas := int32(1)
+			updatedNs := defaultNamespace + "-updated"
+			updatedRemoveNs := true
+			updated.Spec.Replicas = updatedReplicas
+			updated.Spec.Namespace.Name = updatedNs
+			updated.Spec.Namespace.RemoveOnDeletion = updatedRemoveNs
+			Expect(k8sClient.Update(ctx, updated)).Should(Succeed())
+
+			By("Expecting replicas to be updated")
+			Eventually(func() int32 {
+				f := &operatorv1alpha1.Contour{}
+				k8sClient.Get(ctx, key, f)
+				return f.Spec.Replicas
+			}, timeout, interval).Should(Equal(updatedReplicas))
+
+			By("Expecting namespace to be updated")
+			Eventually(func() string {
+				f := &operatorv1alpha1.Contour{}
+				k8sClient.Get(ctx, key, f)
+				return f.Spec.Namespace.Name
+			}, timeout, interval).Should(Equal(updatedNs))
+
+			By("Expecting remove namespace on deletion to be updated")
+			Eventually(func() bool {
+				f := &operatorv1alpha1.Contour{}
+				k8sClient.Get(ctx, key, f)
+				return f.Spec.Namespace.RemoveOnDeletion
+			}, timeout, interval).Should(Equal(updatedRemoveNs))
+
+			By("Expecting to delete contour successfully")
+			Eventually(func() error {
+				f := &operatorv1alpha1.Contour{}
+				k8sClient.Get(ctx, key, f)
+				return k8sClient.Delete(ctx, f)
+			}, timeout, interval).Should(Succeed())
+
+			By("Expecting contour deletion to finish")
+			Eventually(func() error {
+				f := &operatorv1alpha1.Contour{}
+				return k8sClient.Get(ctx, key, f)
+			}, timeout, interval).ShouldNot(Succeed())
+		})
+	})
+})
--- controller/contour/suite_test.go
+++ controller/contour/suite_test.go
@@ -17,15 +17,20 @@ limitations under the License.
 package contour
 
 import (
+	"context"
 	"path/filepath"
 	"testing"
+	"time"
 
 	operatorv1alpha1 "github.com/projectcontour/contour-operator/api/v1alpha1"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -37,9 +42,23 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+// Define utility constants for object names, testing timeouts/durations intervals, etc.
+const (
+	contourName       = "test-contour"
+	operatorNamespace = "test-contour-operator"
+	defaultNamespace  = "projectcontour"
+	defaultReplicas   = int32(2)
+
+	timeout  = time.Second * 10
+	interval = time.Millisecond * 250
+)
+
+var (
+	cfg       *rest.Config
+	k8sClient client.Client
+	testEnv   *envtest.Environment
+	image     = "docker.io/projectcontour/contour:latest"
+)
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -52,9 +71,9 @@ func TestAPIs(t *testing.T) {
 var _ = BeforeSuite(func(done Done) {
 	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
 
-	By("bootstrapping test environment")
+	By("Bootstrapping the test environment")
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths: []string{filepath.Join("..", "..", "config", "crd", "bases")},
 	}
 
 	var err error
@@ -71,11 +90,42 @@ var _ = BeforeSuite(func(done Done) {
 	Expect(err).ToNot(HaveOccurred())
 	Expect(k8sClient).ToNot(BeNil())
 
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		Scheme: scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	By("Creating the reconciler")
+	err = (&Reconciler{
+		Config: Config{
+			Image: image,
+		},
+		Client: k8sClient,
+		Log:    ctrl.Log.WithName("controllers").WithName("Contour"),
+		Scheme: k8sManager.GetScheme(),
+	}).SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
+
+	By("Creating the operator namespace")
+	ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: operatorNamespace}}
+	err = k8sClient.Create(context.Background(), ns)
+	Expect(err).ToNot(HaveOccurred())
 	close(done)
 }, 60)
 
 var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-	err := testEnv.Stop()
+	By("Tearing down the test environment")
+	ns := &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: operatorNamespace}}
+	err := k8sClient.Delete(context.Background(), ns)
+	Expect(err).ToNot(HaveOccurred())
+
+	By("Expecting the test environment teardown to complete")
+	err = testEnv.Stop()
 	Expect(err).ToNot(HaveOccurred())
 })
