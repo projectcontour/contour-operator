d3edaf9008dac517d9ec3b3c4d7a8cf643058b80
Daneyon Hansen <daneyonhansen@gmail.com>
2021-01-29 09:43:31 -0800

    Adds Network Publishing API (#151)
    Adds Network Endpoint Publishing API
    
    Signed-off-by: Daneyon Hansen <daneyonhansen@gmail.com>

--- api/v1alpha1/contour_types.go
+++ api/v1alpha1/contour_types.go
@@ -68,6 +68,13 @@ type ContourSpec struct {
 	//
 	// +kubebuilder:default={name: "projectcontour", removeOnDeletion: false}
 	Namespace NamespaceSpec `json:"namespace,omitempty"`
+
+	// NetworkPublishing defines the schema for publishing Contour to a network.
+	//
+	// See each field for additional details.
+	//
+	// +kubebuilder:default={envoy: {type: LoadBalancerService, containerPorts: {{name: http, portNumber: 8080}, {name: https, portNumber: 8443}}}}
+	NetworkPublishing NetworkPublishing `json:"networkPublishing,omitempty"`
 }
 
 // NamespaceSpec defines the schema of a Contour namespace.
@@ -89,6 +96,162 @@ type NamespaceSpec struct {
 	//
 	// +kubebuilder:default=false
 	RemoveOnDeletion bool `json:"removeOnDeletion,omitempty"`
+}
+
+// NetworkPublishing defines the schema for publishing Contour to a network.
+type NetworkPublishing struct {
+	// Envoy provides the schema for publishing the network endpoints of Envoy.
+	//
+	// If unset, defaults to:
+	//   type: LoadBalancerService
+	//   containerPorts:
+	//   - name: http
+	//     portNumber: 8080
+	//   - name: https
+	//     portNumber: 8443
+	//
+	// +kubebuilder:default={type: LoadBalancerService, loadBalancer: {scope: External, providerParameters: {type: AWS}}, containerPorts: {{name: http, portNumber: 8080}, {name: https, portNumber: 8443}}}
+	Envoy EnvoyNetworkPublishing `json:"envoy,omitempty"`
+}
+
+// EnvoyNetworkPublishing defines the schema to publish Envoy to a network.
+// +union
+type EnvoyNetworkPublishing struct {
+	// Type is the type of publishing strategy to use. Valid values are:
+	//
+	// * LoadBalancerService
+	//
+	// In this configuration, network endpoints for Envoy use container networking.
+	// A Kubernetes LoadBalancer Service is created to publish Envoy network
+	// endpoints. The Service uses port 80 to publish Envoy's HTTP network endpoint
+	// and port 443 to publish Envoy's HTTPS network endpoint.
+	//
+	// See: https://kubernetes.io/docs/concepts/services-networking/service/#loadbalancer
+	//
+	// * NodePortService
+	//
+	// Publishes Envoy network endpoints using a Kubernetes NodePort Service.
+	//
+	// In this configuration, Envoy network endpoints use container networking. A Kubernetes
+	// NodePort Service is created to publish the network endpoints.
+	//
+	// See: https://kubernetes.io/docs/concepts/services-networking/service/#nodeport
+	//
+	// +unionDiscriminator
+	// +kubebuilder:default=LoadBalancerService
+	Type NetworkPublishingType `json:"type,omitempty"`
+
+	// loadBalancer holds parameters for the load balancer. Present only if type is
+	// LoadBalancerService.
+	//
+	// If unspecified, defaults to an external Classic AWS ELB.
+	//
+	// +kubebuilder:default={scope: External, providerParameters: {type: AWS}}
+	LoadBalancer LoadBalancerStrategy `json:"loadBalancer,omitempty"`
+
+	// ContainerPorts is a list of container ports to expose from the Envoy container(s).
+	// Exposing a port here gives the system additional information about the network
+	// connections the Envoy container uses, but is primarily informational. Not specifying
+	// a port here DOES NOT prevent that port from being exposed by Envoy. Any port which is
+	// listening on the default "0.0.0.0" address inside the Envoy container will be accessible
+	// from the network. Names and port numbers must be unique in the list container ports. Two
+	// ports must be specified, one named "http" for Envoy's insecure service and one named
+	// "https" for Envoy's secure service.
+	//
+	// TODO [danehans]: Update minItems to 1, requiring only https when the following issue
+	// is fixed: https://github.com/projectcontour/contour/issues/2577.
+	//
+	// TODO [danehans]: Increase maxItems when https://github.com/projectcontour/contour/pull/3263
+	// is implemented.
+	//
+	// +kubebuilder:validation:MinItems=2
+	// +kubebuilder:validation:MaxItems=2
+	// +kubebuilder:default={{name: http, portNumber: 8080}, {name: https, portNumber: 8443}}
+	ContainerPorts []ContainerPort `json:"containerPorts,omitempty"`
+}
+
+// EndpointPublishingType is a way to publish network endpoints.
+// +kubebuilder:validation:Enum=LoadBalancerService;NodePortService
+type NetworkPublishingType string
+
+const (
+	// LoadBalancerService publishes a network endpoint using a Kubernetes LoadBalancer
+	// Service.
+	LoadBalancerServicePublishingType NetworkPublishingType = "LoadBalancerService"
+
+	// NodePortService publishes a network endpoint using a Kubernetes NodePort Service.
+	NodePortServicePublishingType NetworkPublishingType = "NodePortService"
+)
+
+// LoadBalancerStrategy holds parameters for a load balancer.
+type LoadBalancerStrategy struct {
+	// Scope indicates the scope at which the load balancer is exposed.
+	// Possible values are "External" and "Internal".
+	//
+	// +kubebuilder:default=External
+	Scope LoadBalancerScope `json:"scope,omitempty"`
+
+	// providerParameters contains load balancer information specific to
+	// the underlying infrastructure provider.
+	//
+	// +kubebuilder:default={type: "AWS"}
+	ProviderParameters ProviderLoadBalancerParameters `json:"providerParameters,omitempty"`
+}
+
+// LoadBalancerScope is the scope at which a load balancer is exposed.
+// +kubebuilder:validation:Enum=Internal;External
+type LoadBalancerScope string
+
+var (
+	// InternalLoadBalancer is a load balancer that is exposed only on the
+	// cluster's private network.
+	InternalLoadBalancer LoadBalancerScope = "Internal"
+
+	// ExternalLoadBalancer is a load balancer that is exposed on the
+	// cluster's public network (which is typically on the Internet).
+	ExternalLoadBalancer LoadBalancerScope = "External"
+)
+
+// ProviderLoadBalancerParameters holds desired load balancer information
+// specific to the underlying infrastructure provider.
+//
+// +union
+type ProviderLoadBalancerParameters struct {
+	// type is the underlying infrastructure provider for the load balancer.
+	// Allowed values are "AWS", "Azure", and "GCP".
+	//
+	// +unionDiscriminator
+	// +kubebuilder:default=AWS
+	Type LoadBalancerProviderType `json:"type,omitempty"`
+}
+
+// LoadBalancerProviderType is the underlying infrastructure provider for the
+// load balancer. Allowed values are "AWS", "Azure", and "GCP".
+//
+// +kubebuilder:validation:Enum=AWS;Azure;GCP
+type LoadBalancerProviderType string
+
+const (
+	AWSLoadBalancerProvider   LoadBalancerProviderType = "AWS"
+	AzureLoadBalancerProvider LoadBalancerProviderType = "Azure"
+	GCPLoadBalancerProvider   LoadBalancerProviderType = "GCP"
+)
+
+// ContainerPort is the schema to specify a network port for a container.
+// A container port gives the system additional information about network
+// connections a container uses, but is primarily informational.
+type ContainerPort struct {
+	// Name is an IANA_SVC_NAME within the pod.
+	//
+	// +kubebuilder:validation:MinLength=1
+	// +kubebuilder:validation:MaxLength=253
+	Name string `json:"name"`
+
+	// PortNumber is the network port number to expose on the envoy pod.
+	// The number must be greater than 0 and less than 65536.
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=65535
+	PortNumber int32 `json:"portNumber"`
 }
 
 const (
