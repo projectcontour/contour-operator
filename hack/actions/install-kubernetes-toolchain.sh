9c23e386fac6761fbb6ce06f104330b8f2ecc032
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2021-04-29 18:14:22 -0400

    Test against k8s 1.21.0 in example and e2e tests (#328)
    * Use latest kind and steveslokas kind image in CI
    
    Should be reverted/changed once there is an upstream 1.21.0 image
    and new kind version
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    
    * Bring over NODEIMAGE logic
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    
    * Some cleanups found when running tests locally
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    
    * Fix kubebuilder download
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    
    * Set grace period on test created pods to 0
    
    Also reduce duplicaton of namespace deletion/wait for deletion functions
    
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- hack/actions/install-kubernetes-toolchain.sh
+++ hack/actions/install-kubernetes-toolchain.sh
@@ -38,12 +38,20 @@ case "$#" in
     ;;
 esac
 
+# TODO: Remove once upstream images are available (https://github.com/projectcontour/contour/issues/3610).
+# Install latest version of kind.
+go get sigs.k8s.io/kind@master
 
-download \
-    "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERS}/kind-${OS}-amd64" \
-    "${DESTDIR}/kind"
+# Move the $GOPATH/bin/kind binary to local since Github actions
+# have their own version installed.
+mv /home/runner/go/bin/kind ${DESTDIR}/kind
 
-chmod +x  "${DESTDIR}/kind"
+# Uncomment this once v0.11 of Kind is released.
+# download \
+#     "https://github.com/kubernetes-sigs/kind/releases/download/${KIND_VERS}/kind-${OS}-amd64" \
+#     "${DESTDIR}/kind"
+
+# chmod +x  "${DESTDIR}/kind"
 
 download \
     "https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERS}/bin/${OS}/amd64/kubectl" \
@@ -58,6 +66,9 @@ download \
 tar -C "${DESTDIR}" -xf "${DESTDIR}/kustomize.tgz" kustomize
 rm "${DESTDIR}/kustomize.tgz"
 
-curl -L https://go.kubebuilder.io/dl/${KUBEBUILDER_VERS}/${OS}/amd64 | tar -xz -C /tmp/
+download \
+    "https://github.com/kubernetes-sigs/kubebuilder/releases/download/v${KUBEBUILDER_VERS}/kubebuilder_${KUBEBUILDER_VERS}_${OS}_amd64.tar.gz" \
+    "/tmp/kubebuilder.tgz"
+tar -C /tmp/ -xzf /tmp/kubebuilder.tgz
 mv /tmp/kubebuilder_${KUBEBUILDER_VERS}_${OS}_amd64 kubebuilder
 sudo mv kubebuilder /usr/local/
