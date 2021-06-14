2ff107e7821d43b658efb272be7dad1cfa491836
Steve Kriss <krisss@vmware.com>
2021-06-14 13:57:49 -0600

    CI: add uncommitted codegen check
    Adds a CI check that runs all code generation Make tasks
    and then errors if there are any uncommitted changes,
    meaning the PR should've updated generated code but did
    not.
    
    Closes #384.
    
    Signed-off-by: Steve Kriss <krisss@vmware.com>

---
+++ hack/actions/check-uncommitted-codegen.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly HERE=$(cd $(dirname $0) && pwd)
+readonly REPO=$(cd ${HERE}/../.. && pwd)
+
+if git status -s ${REPO} 2>&1 | grep -E -q '^\s+[MADRCU]'
+then
+	echo Uncommitted changes in generated sources:
+	git status -s ${REPO}
+	exit 1
+fi
