b4a03c2bcd58fe6cbf85db91fdeb3136710f9224
Steve Sloka <slokas@vmware.com>
2021-10-26 10:32:22 -0400

    Update Contour Docker image to v1.19.1.
    Signed-off-by: Steve Sloka <slokas@vmware.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,7 +14,7 @@
 package config
 
 const (
-	DefaultContourImage           = "ghcr.io/projectcontour/contour:v1.19.0"
+	DefaultContourImage           = "ghcr.io/projectcontour/contour:v1.19.1"
 	DefaultEnvoyImage             = "docker.io/envoyproxy/envoy:v1.19.1"
 	DefaultMetricsAddr            = ":8080"
 	DefaultEnableLeaderElection   = false
