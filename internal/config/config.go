59f513739c44daa9db4eb937b8311be53c96d63b
Sunjay Bhatia <5337253+sunjayBhatia@users.noreply.github.com>
2022-02-24 16:41:43 -0500

    update Envoy to v1.21.1 (#496) (#497)
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>
    Signed-off-by: Steve Kriss <krisss@vmware.com>
    Co-authored-by: Steve Kriss <krisss@vmware.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -15,7 +15,7 @@ package config
 
 const (
 	DefaultContourImage           = "ghcr.io/projectcontour/contour:v1.20.0"
-	DefaultEnvoyImage             = "docker.io/envoyproxy/envoy:v1.21.0"
+	DefaultEnvoyImage             = "docker.io/envoyproxy/envoy:v1.21.1"
 	DefaultMetricsAddr            = ":8080"
 	DefaultEnableLeaderElection   = false
 	DefaultEnableLeaderElectionID = "0d879e31.projectcontour.io"
