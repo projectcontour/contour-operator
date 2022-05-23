693032d3228987e35ec84182c7b9b88ce352c247
Steve Kriss <krisss@vmware.com>
2022-05-23 22:24:09 +0000

    fix lint issues
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- internal/status/conditions.go
+++ internal/status/conditions.go
@@ -22,7 +22,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	utilclock "k8s.io/apimachinery/pkg/util/clock"
+	utilclock "k8s.io/utils/clock"
 )
 
 // clock is to enable unit testing
