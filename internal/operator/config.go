10d18e96c938f90d6a0bff5b1e1a3b22d01598ea
Sunjay Bhatia <sunjayb@vmware.com>
2022-06-14 16:10:36 +0000

    Update Contour Docker image to v1.21.1.
    Signed-off-by: Sunjay Bhatia <sunjayb@vmware.com>

--- internal/operator/config.go
+++ internal/operator/config.go
@@ -14,7 +14,7 @@
 package operator
 
 const (
-	DefaultContourImage           = "ghcr.io/projectcontour/contour:v1.21.0"
+	DefaultContourImage           = "ghcr.io/projectcontour/contour:v1.21.1"
 	DefaultEnvoyImage             = "docker.io/envoyproxy/envoy:v1.22.2"
 	DefaultMetricsAddr            = ":8080"
 	DefaultEnableLeaderElection   = false
