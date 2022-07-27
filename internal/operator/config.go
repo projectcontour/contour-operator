9d7fa35eacd72653781fc5298f86caa46d4d2f2d
Steve Kriss <krisss@vmware.com>
2022-07-27 17:33:22 +0000

    Update Contour Docker image to v1.22.0.
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- internal/operator/config.go
+++ internal/operator/config.go
@@ -14,7 +14,7 @@
 package operator
 
 const (
-	DefaultContourImage           = "ghcr.io/projectcontour/contour:main"
+	DefaultContourImage           = "ghcr.io/projectcontour/contour:v1.22.0"
 	DefaultEnvoyImage             = "docker.io/envoyproxy/envoy:v1.23.0"
 	DefaultMetricsAddr            = ":8080"
 	DefaultEnableLeaderElection   = false
