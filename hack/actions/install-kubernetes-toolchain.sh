0830553ad7170c0423c3c6d1f64d7b2b9fac2e8b
Steve Kriss <krisss@vmware.com>
2022-12-21 16:31:26 +0000

    update kubectl and kind
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- hack/actions/install-kubernetes-toolchain.sh
+++ hack/actions/install-kubernetes-toolchain.sh
@@ -5,8 +5,8 @@ set -o nounset
 set -o pipefail
 
 readonly KUSTOMIZE_VERS="v3.5.4"
-readonly KUBECTL_VERS="v1.24.0"
-readonly KIND_VERS="v0.14.0"
+readonly KUBECTL_VERS="v1.26.0"
+readonly KIND_VERS="v0.17.0"
 readonly KUBEBUILDER_VERS="2.3.1"
 
 readonly PROGNAME=$(basename $0)
